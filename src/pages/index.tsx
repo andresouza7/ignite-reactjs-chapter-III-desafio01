e5ae33ed92308ba322f97580af7705f944c4e109
Andre Souza <souza.andre1993@gmail.com>
2021-03-26 12:24:06 -0300

    implement query posts from prismic, working on home page style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { GetStaticProps } from 'next';
 
+import Prismic from '@prismicio/client';
+import Link from 'next/link';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,13 +26,70 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  // TODO
+  console.log(postsPagination);
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+  return (
+    <main className={styles.homeContainer}>
+      {postsPagination.results.map(post => (
+        <div key={post.uid} className={styles.postItem}>
+          <Link href="/">
+            <a>{post.data.title}</a>
+          </Link>
+          <p>{post.data.subtitle}</p>
+          <time>{post.first_publication_date}</time>
+          <span>{post.data.author}</span>
+        </div>
+      ))}
+    </main>
+  );
+}
 
-//   // TODO
-// };
+export const getStaticProps: GetStaticProps = async () => {
+  // const prismic = getPrismicClient();
+  // const postsResponse = await prismic.query(
+  //   [Prismic.predicates.at('document.type', 'post')],
+  //   {
+  //     fetch: ['post.title', 'post.subtitle', 'post.author'],
+  //     pageSize: 1,
+  //   }
+  // );
+
+  // const posts: Post[] = postsResponse?.results.map(post => ({
+  //   uid: post.uid,
+  //   first_publication_date: post.first_publication_date,
+  //   data: {
+  //     title: post.data.title,
+  //     subtitle: post.data.subtitle,
+  //     author: post.data.author,
+  //   },
+  // }));
+
+  const postsPagination = {
+    next_page:
+      'https://ignite-blog-app.cdn.prismic.io/api/v2/documents/search?ref=YF3tixMAACAAPVi9&q=%5B%5Bat%28document.type%2C+%22post%22%29%5D%5D&page=2&pageSize=1',
+    results: [
+      {
+        uid: 'spacex-rocket-debris-creates-a-fantastic-light-show',
+        first_publication_date: '2021-03-26T14:19:55+0000',
+        data: {
+          title:
+            'SpaceX rocket debris creates a fantastic light show in the Pacific Northwest sky',
+          subtitle: 'Not a meteor shower, but space age spoilage',
+          author: 'James Vincent',
+        },
+      },
+    ],
+  };
+
+  // const postsPagination: PostPagination = {
+  //   next_page: postsResponse.next_page,
+  //   results: posts,
+  // };
+
+  return {
+    props: { postsPagination },
+    revalidate: 60 * 10, // 10 min
+  };
+};
